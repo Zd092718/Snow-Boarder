773aec481e57ba1cbf84025c0bf88cc9e51da65e
Zachary Dowd <zacharyd2796@zohomail.com>
2022-05-19 13:41:17 -0600

    Finished prototype level

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -8,10 +8,12 @@ public class CrashDetector : MonoBehaviour
     [SerializeField] float delayTime = .5f;
     [SerializeField] ParticleSystem deathParticles;
     [SerializeField] AudioClip crashSFX;
+    bool hasCrashed = false;
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Ground")
+        if (other.gameObject.tag == "Ground" && !hasCrashed)
         {
+            hasCrashed = true;
             FindObjectOfType<PlayerController>().DisableControls();
             deathParticles.Play();
             GetComponent<AudioSource>().PlayOneShot(crashSFX);
