054d9b3e6007c89c6a847be76b9cf189e3fdfd51
Zachary Dowd <zacharyd2796@zohomail.com>
2022-05-19 02:51:21 -0600

    added game finish functionality

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float delayTime = 1f;
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.gameObject.tag == "Ground")
+        {
+            Invoke("ReloadScene", delayTime);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void ReloadScene()
     {
-        
+        SceneManager.LoadScene(0);
+
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] float delayTime = 1f;
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.gameObject.tag == "Player")
+        {
+            Invoke("ReloadScene", delayTime);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void ReloadScene()
     {
-        
+        SceneManager.LoadScene(0);
     }
 }
